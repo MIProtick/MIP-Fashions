9107d05f8999fcd5c32bcf14fbd8ebaa57eb4bc0
MIProtick <mezbaulislamprotick@outlook.com>
2021-05-08 15:49:47 +0600

    signin with email and password is completed.

--- src/components/signIn/SignIn.component.jsx
+++ src/components/signIn/SignIn.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import FormInput from '../form-input/FormInput.component';
 import CustomButton from '../custom-button/CustomButton.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './SignIn.style.scss';
 
 export class SignIn extends Component {
@@ -14,9 +14,16 @@ export class SignIn extends Component {
     }
   }
   
-  handleSubmit = event =>{
+  handleSubmit = async event =>{
     event.preventDefault();
-    this.setState({email:'', password:''});
+    
+    const {email, password} = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email:'', password:''});
+    } catch (error) {
+      console.log("Error occured in Sign-in process.")
+    }
   }
   
   handleChange = event =>{
