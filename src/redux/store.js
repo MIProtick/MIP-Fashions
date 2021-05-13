c75bcb8a3e55d0a0e241b4c2afd613ae770203db
MIProtick <mezbaulislamprotick@outlook.com>
2021-05-13 11:53:40 +0600

    intoduced usages of local storage with redux-persist.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import RootReducer from './RootReducer';
 
 const middlewares = [logger];
-const store = createStore(RootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const store = createStore(RootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
+
+
+const defStore = { store, persistor }
+export default defStore;
